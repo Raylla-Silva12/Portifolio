834a73eb62344ff496031a2a3d674a1773a018c7
Raylla-Silva12 <raylla.l.silva@gmail.com>
2024-04-23 15:32:38 -0300

    Versao 0.1

---
+++ api/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap and CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+    <title>Portifólio - Raylla Silva</title>
+</head>
+
+<body>
+
+    <div class="main">
+        <div class="header">
+            <div>
+                <h1>
+                    <span class="titulo">Desenvolvedora </span>
+                    <span contenteditable="true" class="titulo-fullstack"> Fullstack :D </span>
+                </h1>
+
+                <p class="desc-curta">Meu nome é Raylla Silva, tenho 19 anos, sou desenvolvedora fullstack e encantada por tecnologia e inovação.</p>
+
+                <div class="contato">
+                    <a href="https://github.com/Raylla-Silva12" target="_blank"><button class="glow-on-hover" type="button">GitHub</button></a>
+                    <a href="https://www.linkedin.com/in/raylla-lopes-silva-50554220b/" target="_blank"><button class="glow-on-hover" type="button">Linkedin</button></a>
+                </div>
+
+                <a href="#sobre-mim"><img class="cursor" src="img/seta.png" alt="cursor"></a>
+            </div>
+        </div>
+
+        <div class="sobre-mim" id="sobre-mim">
+            <div class="row">
+                <div class="col-md txt">
+                    <h1>Sobre mim</h1>
+                    <p>Me chamo Raylla e sou completamente encantada por tecnologia, em específico front e back-end. Comecei com a linguagem PHP em cursos técnicos em 2020 e a partir daí não parei de me especializar em tecnologia da informação.</p>
+                    <p>Atualmente prossigo me com a minha formação de nível superior em TI na Univesp e realizando cursos de especialização no Santander Open Academy e DIO.</p>
+                    <p class="txt-estudos">Atualmente estudando <span class="linguagem">Python</span>, <span class="linguagem">JAVA</span> e <span class="linguagem">AWS</span>.</p>
+                </div>
+                <div class="col-md">
+                    <img class="img-sobre-mim" src="img/eu.png">
+                </div>
+            </div>
+        </div>
+
+        <div class="projetos">
+            <div class="row">
+                <h1 class="card-h1">Projetos preferidos</h1>
+                <div class="col-md">
+                    <div class="card" style="width: 25rem;">
+                        <a href="https://github.com/Raylla-Silva12/PW_RedeSocial">
+                            <img src="img/evagas.jpg" class="card-img-top" alt="evagas">
+                        </a>
+
+                        <div class="card-body">
+                            <p class="card-text">Site com estilo de rede social para a divulgação de empregos e busca de vagas.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card" style="width: 25rem;">
+                        <a href="https://github.com/Raylla-Silva12/TCC_REYNTECH">
+                            <img src="img/reyntech.jpg" class="card-img-top" alt="reyntech">
+                        </a>
+                        <div class="card-body">
+                            <p class="card-text">Site e-commerce para a venda de produtos de botânica e divulgação de informações.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
